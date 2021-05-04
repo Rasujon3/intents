9042d133a10599403d88e569dd6a71f27d4f2e89
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-04 21:39:35 +0600

    1_What is Intent - Android Development with Java - Rakibul Islam - Bohubrihi
    1_What is Intent - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/intents/MainActivity.java
+++ app/src/main/java/com/sujon/intents/MainActivity.java
@@ -1,5 +1,8 @@
 package com.sujon.intents;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onMyButtonClick(View view){
+//        Toast.makeText(this,"Button click",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(intent);
+    }
+
 }
