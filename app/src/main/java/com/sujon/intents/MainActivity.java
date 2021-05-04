09d76463ef56b9ddff5c10b4ea1b60a21b9db9f5
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-04 23:01:30 +0600

    2_Send Intent Data - Android Development with Java - Rakibul Islam - Bohubrihi
    2_Send Intent Data - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/intents/MainActivity.java
+++ app/src/main/java/com/sujon/intents/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String intentVariableName = "MY_STRING_FIRST";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public void onMyButtonClick(View view){
 //        Toast.makeText(this,"Button click",Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        intent.putExtra(intentVariableName,"WELCOME_TO_SECOND_ACTIVITY");
         startActivity(intent);
     }
 
--- app/src/main/java/com/sujon/intents/MainActivity2.java
+++ app/src/main/java/com/sujon/intents/MainActivity2.java
@@ -1,13 +1,21 @@
 package com.sujon.intents;
 
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    TextView myTextView;
+    String first_string;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        myTextView = findViewById(R.id.first_text_view);
+        first_string = getIntent().getStringExtra("MY_STRING_FIRST");
+        myTextView.setText(first_string);
     }
 }
